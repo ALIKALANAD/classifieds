21224260a002535ab8d6a05b3c7ae6c2045dbb0e
Jurel P <patocjurel@gmail.com>
2015-11-26 05:52:02 +0800

    Password resets updated

---
+++ resources/views/auth/password.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="container">
+
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-default">
+                <div class="panel-body">
+
+                    <legend>Forgot Password</legend>
+
+                    @include('common.errors')
+
+                    {!! Form::open(['url' => '/password/email', 'method' => 'post', 'class' => 'form-horizontal', 'role' => 'form']) !!}
+                        {!! csrf_field() !!}
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Email</label>
+                            <div class="col-md-6">
+                                {!! Form::text('email', old('email'), ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
+                            </div>
+                        </div>
+
+                    {!! Form::close() !!}
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+@endsection
--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -1,31 +1,20 @@
 @extends('layouts.master')
 
 @section('content')
+
     <div class="container">
 
         <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-default">
                 <div class="panel-body">
 
-                    <?php
-                        /*
-                        $ip = (object) GeoIP::getLocation("14.203.96.226");
-                        echo $ip->ip . "<br>";
-                        echo $ip->isoCode . "<br>";
-                        echo $ip->country . "<br>";
-                        echo $ip->city . "<br>";
-                        echo $ip->state . "<br>";
-                        echo $ip->postal_code . "<br>";
-                        echo $ip->timezone . "<br>";
-                        echo $ip->continent . "<br>";
-                        */
-                    ?>
-
-                    <legend>User Sign in</legend>
+                    <legend>New Password</legend>
 
                     @include('common.errors')
 
-                    {!! Form::open(['url' => '/auth/login', 'method' => 'post', 'class' => 'form-horizontal', 'role' => 'form']) !!}
+                    {!! Form::open(['url' => '/password/reset', 'method' => 'post', 'class' => 'form-horizontal', 'role' => 'form']) !!}
+
+                        {!! Form::hidden('token', $token) !!}
 
                         <div class="form-group">
                             <label class="col-md-4 control-label">Email</label>
@@ -35,21 +24,22 @@
                         </div>
 
                         <div class="form-group">
-                            <label class="col-md-4 control-label">Password</label>
+                            <label class="col-md-4 control-label">New Password</label>
                             <div class="col-md-6">
                                 {!! Form::password('password', ['class' => 'form-control']) !!}
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <a href="{{ url('password/email') }}">Forgot Password?</a>
+                            <label class="col-md-4 control-label">Retype New Password</label>
+                            <div class="col-md-6">
+                                {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
                             </div>
                         </div>
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
+                                {!! Form::submit('Submit', array('class' => 'btn primary')) !!}
                             </div>
                         </div>
 
@@ -60,4 +50,5 @@
         </div>
 
     </div>
+
 @endsection
